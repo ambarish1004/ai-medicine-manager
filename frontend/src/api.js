e6b7d3acd9bb966e7eb29d2d90cf3460ad13c2eb
ambarish1004 <142644804+ambarish1004@users.noreply.github.com>
2026-03-23 15:09:32 +0530

    changed local URL to render's deployed backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8001";
+// const BASE_URL = "http://127.0.0.1:8001";
+const BASE_URL = "https://ai-medicine-manager-backend.onrender.com";
 
 export const addMedicine = async (data) => {
   return fetch(`${BASE_URL}/medicine/add`, {
